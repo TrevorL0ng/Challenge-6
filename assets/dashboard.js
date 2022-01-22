f6c58f740177fb9ee3f24ba14ddd73893ce4ef8f
Trevor L <trevorlong992@gmail.com>
2022-01-22 18:54:23 -0500

    Sudden brain sludge. Need a break. Pushing up what I have now. Be back soon.

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -4,3 +4,10 @@ var gazeNow="api.openweathermap.org/data/2.5/weather?q="
 var gazeUV="api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}"
 var gazeFive="api.openweathermap.org/data/2.5/forecast?q="
 
+$(document).ready(function() {
+    $("gaze-button").on("click", function() {
+        var desiredGaze = $("gaze-box");
+        skySight(desiredGaze);
+    })
+})
+
