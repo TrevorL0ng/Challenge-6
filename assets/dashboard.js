4f979a1bcc3e2d21a5519d80e9e2dbfdc488edae
Trevor L <trevorlong992@gmail.com>
2022-01-22 18:36:28 -0500

    Global variables set for the most part. I believe.

---
+++ assets/dashboard.js
@@ -0,0 +1,6 @@
+var apiKey="85b3199b8c20c1ee1c5c37943d8fc461"
+var home="concord"
+var gazeNow="api.openweathermap.org/data/2.5/weather?q="
+var gazeUV="api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}"
+var gazeFive="api.openweathermap.org/data/2.5/forecast?q="
+
