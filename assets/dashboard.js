e2c1e5b88ad9a3f4c9b3de513e44d6b3a3c4682c
Trevor L <trevorlong992@gmail.com>
2022-02-05 14:17:28 -0500

    Keep moving

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,6 +1,6 @@
 var apiKey="85b3199b8c20c1ee1c5c37943d8fc461"
 var home="concord"
-var gazeNow="api.openweathermap.org/data/2.5/weather?q="
+var gazeNow="api.openweathermap.org/data/2.5/weather?appid="
 var gazeUV="api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}"
 var gazeFive="api.openweathermap.org/data/2.5/forecast?q="
 
@@ -11,3 +11,22 @@ $(document).ready(function() {
     })
 })
 
+function skySight(location){
+    var callFunc = ""
+    if (location !== ""){
+        callFunc = gazeNow + apiKey + "&q=" + location
+    }
+    else {
+        callFunc = gazeNow + apiKey +"&q=" + home
+    }
+}
+
+$.ajax({
+    url: callFunc,
+    method: "GET"
+})
+.then (function(response) {
+    console.log(response)
+    var tempur = response.main.temp
+    $("#skyNow").append("<h3>" + tempur + "</h3>")
+})    
