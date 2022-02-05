c56184863c75489f9364a1e3c68b1b3b319950d6
Trevor L <trevorlong992@gmail.com>
2022-02-05 15:14:08 -0500

    In a passable state. Current temp shows, general concept achieved of being able to fetch and show serverside data. On to the next one.

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,25 +1,25 @@
 var apiKey="85b3199b8c20c1ee1c5c37943d8fc461"
 var home="concord"
-var gazeNow="api.openweathermap.org/data/2.5/weather?appid="
-var gazeUV="api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}"
-var gazeFive="api.openweathermap.org/data/2.5/forecast?q="
+var gazeNow="https://api.openweathermap.org/data/2.5/weather?appid="
+var gazeUV="https://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}"
+var gazeFive="https://api.openweathermap.org/data/2.5/forecast?q="
 
 $(document).ready(function() {
-    $("gaze-button").on("click", function() {
-        var desiredGaze = $("gaze-box");
+    $("#gaze-button").on("click", function(event) {
+        var desiredGaze = $("#gaze-box").val();
         skySight(desiredGaze);
     })
 })
 
 function skySight(location){
-    var callFunc = ""
+    var callFunc = "";
     if (location !== ""){
-        callFunc = gazeNow + apiKey + "&q=" + location
+        callFunc = gazeNow + apiKey + "&q=" + location + "&units=imperial"
     }
     else {
-        callFunc = gazeNow + apiKey +"&q=" + home
+        callFunc = gazeNow + apiKey +"&q=" + home + "&units=imperial"
     }
-}
+
 
 $.ajax({
     url: callFunc,
@@ -27,6 +27,8 @@ $.ajax({
 })
 .then (function(response) {
     console.log(response)
-    var tempur = response.main.temp
-    $("#skyNow").append("<h3>" + tempur + "</h3>")
+    var tempur = response.main.temp;
+    var here = response.name
+    $(".skyNow").append("The temperature is " + tempur + " Fahrenheit in " + here)
 })    
+}
